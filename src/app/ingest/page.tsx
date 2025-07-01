47f7f9607e997298ee089656e8d1ee779ff2baf8
Mudit Nag <muditnag13@gmail.com>
2025-07-01 23:57:25 +0530

    icon removed

--- src/app/ingest/page.tsx
+++ src/app/ingest/page.tsx
@@ -72,22 +72,7 @@ export default function IngestPage() {
                   : "text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white"
               }`}
             >
-              <span className="flex items-center">
-                <svg
-                  className="w-4 h-4 mr-1"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 100 4m0-4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 100 4m0-4v2m0-6V4"
-                  />
-                </svg>
-                Manage
-              </span>
+              <span className="flex-1 items-center">Manage</span>
             </button>
           </div>
 
